499b7f641f8512a56a2ac8ad01d4a0e1483021f0
INDIANA SASSON <48043270@ort>
2025-07-08 15:17:08 -0300

    bdd

---
+++ src/data/Bdd.js
@@ -0,0 +1,24 @@
+import DBConfig from "./DBConfig";
+
+
+/*import pkg from 'pg'
+const { Client, Pool } = pkg;
+
+
+export default class Province Repository {
+    getAllAsync = async () => {
+        let returnArray = null;
+        const client = new Client (DBConfig);
+        try {
+            await client.connect();
+            const sql = "SELECT * FROM provinces';
+            const result await client.query(sql);
+            await client.end();
+            returnArray = result.rows;
+        } catch (error) {
+            console.log(error);
+        }
+        return returnArray;
+}
+*/
+
